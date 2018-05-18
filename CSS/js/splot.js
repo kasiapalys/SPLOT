9e72ea1e1af9249e86b5a0b3f40800c2a629f8fd
katkawariatka <33699605+katkawariatka@users.noreply.github.com>
2018-05-18 13:01:07 +0100

    :sparkles: initial commit

---
+++ CSS/js/splot.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // $('.zoom').addClass('animated bounce');
+  $('.zoom').zoom({});
+
+  $('.sizePicture').on('click'; 'function')
+
+  $('.sizePicture').on('click', function() {
+    // $('.picture').attr('src', './focusson-images/analizowany.png')
+    
+
+});
