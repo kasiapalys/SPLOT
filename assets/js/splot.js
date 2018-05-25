f2397c65775e0704608bc2cbb431d694a7ca83bd
Katarzyna Palys <palys.katarzyna@gmail.com>
2018-05-25 14:57:56 +0100

    popup

--- assets/js/splot.js
+++ assets/js/splot.js
@@ -9,7 +9,15 @@ $(document).ready(function(){
     $('.bigPicture').attr('src', sciezka)
   })
 
-    var name = $('.productName').text()
-    $('.inputSize').attr('value', name)
+  var name = $('.productName').text()
+  $('.inputSize').attr('value', name)
+
+     // Initialize the plugin http://dev.vast.com/jquery-popup-overlay/
+  $('#my_popup').popup({
+    autoopen: true,
+  })
   
+  $('.popupMain .popupClose,.popupGrey').on('click', function() {
+    $('.popupLayout').fadeOut(1000)
+  })
 });
